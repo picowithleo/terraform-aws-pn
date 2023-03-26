80f0bbf0483f0a312944397ba40ba91388894f4d
Jinyuan Chen <picowithleo@gmail.com>
2023-03-27 03:01:11 +1000

    Created cloudfront modules

---
+++ modules/cloudfront/main.tf
@@ -0,0 +1,55 @@
+resource "aws_cloudfront_distribution" "this" {
+  origin {
+    domain_name = var.origin_domain_name
+    origin_id   = var.origin_id
+
+    s3_origin_config {
+      origin_access_identity = ""
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = var.aliases
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = var.origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = var.acm_certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
