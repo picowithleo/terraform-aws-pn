b9128b79f8ad064ef63ef75d6e483a319a6a6b1a
Jinyuan Chen <picowithleo@gmail.com>
2023-03-28 22:52:55 +1000

    Modified bucket name variables

--- modules/s3_bucket/variables.tf
+++ modules/s3_bucket/variables.tf
@@ -1,4 +1,14 @@
-variable "bucket_name" {
+# variable "bucket_name" {
+#   type        = string
+#   description = "The name of the S3 bucket"
+# }
+
+variable "bucket_name_prefix" {
+  description = "The prefix for the S3 bucket name"
   type        = string
-  description = "The name of the S3 bucket"
+}
+
+variable "domain_name" {
+  description = "The domain name for the S3 bucket"
+  type        = string
 }
