d449398411feef00549fdf4db421e8929f3b320a
Jinyuan Chen <picowithleo@gmail.com>
2023-03-28 22:54:16 +1000

    Modified dns modules

--- modules/dns/main..tf
+++ modules/dns/main..tf
@@ -1,13 +1,54 @@
-resource "aws_route53_record" "this" {
-  name             = var.name
-  type             = var.record_type
-  zone_id          = var.zone_id
-  records          = var.record_type == "A" ? null : [var.target_record]
-  ttl              = var.record_type == "A" ? null : 60
+# resource "aws_route53_record" "this" {
+#   name             = var.name
+#   type             = var.record_type
+#   zone_id          = var.zone_id
+#   records          = var.record_type == "A" ? null : [var.target_record]
+#   ttl              = var.record_type == "A" ? null : 60
 
-  alias {
-    name                   = var.record_type == "A" ? var.target_domain : null
-    zone_id                = var.record_type == "A" ? var.target_zone_id : null
-    evaluate_target_health = false
+#   alias {
+#     name                   = var.record_type == "A" ? var.target_domain : null
+#     zone_id                = var.record_type == "A" ? var.target_zone_id : null
+#     evaluate_target_health = false
+#   }
+# }
+
+# resource "aws_route53_record" "this" {
+#   zone_id = var.zone_id
+#   name    = var.name
+#   type    = var.record_type
+#   ttl     = "300"
+
+#   dynamic "records" {
+#     for_each = var.record_values
+#     content {
+#       value = records.value
+#     }
+#   }
+
+#   dynamic "alias" {
+#     for_each = var.alias_target != null ? [1] : []
+#     content {
+#       name                   = var.alias_target.name
+#       zone_id                = var.alias_target.zone_id
+#       evaluate_target_health = var.alias_target.evaluate_target_health
+#     }
+#   }
+# }
+
+resource "aws_route53_record" "this" {
+  zone_id = var.zone_id
+  name    = var.name
+  type    = var.record_type
+
+  dynamic "alias" {
+    for_each = var.alias_target != null ? [var.alias_target] : []
+    content {
+      name                   = alias.value.name
+      zone_id                = alias.value.zone_id
+      evaluate_target_health = alias.value.evaluate_target_health
+    }
   }
+
+  records = var.record_values
+  ttl     = "300"
 }
--- modules/dns/variables.tf
+++ modules/dns/variables.tf
@@ -13,20 +13,36 @@ variable "zone_id" {
   description = "The Route53 zone ID for the domain"
 }
 
-variable "target_domain" {
-  type        = string
-  description = "The target domain name for the alias record (A type)"
-  default     = ""
+# variable "target_domain" {
+#   type        = string
+#   description = "The target domain name for the alias record (A type)"
+#   default     = ""
+# }
+
+# variable "target_zone_id" {
+#   type        = string
+#   description = "The target hosted zone ID for the alias record (A type)"
+#   default     = ""
+# }
+
+# variable "target_record" {
+#   type        = string
+#   description = "The target record value for the CNAME record (CNAME type)"
+#   default     = ""
+# }
+
+variable "record_values" {
+  description = "A string list of records. The number of items depends on the type. For example, an A record would have exactly one value."
+  type        = list(string)
+  default     = []
 }
 
-variable "target_zone_id" {
-  type        = string
-  description = "The target hosted zone ID for the alias record (A type)"
-  default     = ""
-}
-
-variable "target_record" {
-  type        = string
-  description = "The target record value for the CNAME record (CNAME type)"
-  default     = ""
+variable "alias_target" {
+  description = "Configuration block indicating the target for an alias record."
+  type = object({
+    name                   = string
+    zone_id                = string
+    evaluate_target_health = bool
+  })
+  default = null
 }
