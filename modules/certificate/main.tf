b419beb4482aa2616fd01eb56a9e443264ea6b36
Jinyuan Chen <picowithleo@gmail.com>
2023-03-28 22:54:45 +1000

    Modified certificate modules and added version.tf

--- modules/certificate/main.tf
+++ modules/certificate/main.tf
@@ -1,5 +1,4 @@
 resource "aws_acm_certificate" "this" {
-  provider          = aws.us
   domain_name       = var.domain_name
   validation_method = "DNS"
 
@@ -13,28 +12,43 @@ resource "aws_acm_certificate" "this" {
 }
 
 resource "aws_acm_certificate_validation" "this" {
-  provider                = aws.us
   certificate_arn         = aws_acm_certificate.this.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation_records : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
 
-locals {
-  validation_records = {
+# locals {
+#   validation_records = {
+#     for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
+# }
+
+# resource "aws_route53_record" "validation_records" {
+#   for_each = local.validation_records
+
+#   allow_overwrite = true
+#   name             = each.value.name
+#   type             = each.value.type
+#   zone_id          = var.zone_id
+#   records          = [each.value.record]
+#   ttl              = 60
+# }
+resource "aws_route53_record" "validation" {
+  for_each = {
     for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
   }
-}
-
-resource "aws_route53_record" "validation_records" {
-  for_each = local.validation_records
 
   allow_overwrite = true
-  name             = each.value.name
-  type             = each.value.type
-  zone_id          = var.zone_id
-  records          = [each.value.record]
-  ttl              = 60
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.zone_id
 }
